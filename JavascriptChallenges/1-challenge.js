1b5b3a35b071044f946e3101c098f76b3aab4df5
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-03-12 15:33:31 +0100

    my first challenge

---
+++ JavascriptChallenges/1-challenge.js
@@ -0,0 +1,25 @@
+const myBoolean = true
+
+const myString = 'hello world';
+
+const firstNumber = 20;
+
+const secondNumber = 40;
+
+const firstNumber = 20;
+
+secondNumber = 80;
+
+const myArray = [myBoolean, myString];
+
+const myObject = { firstProperty: myString, sumProperty: firstNumber + secondNumber}
+
+console.log(myObject);
+
+console.log(myObject.sumProperty);
+
+//9
+
+console.log(myObject.firstProperty);
+
+
